e1bda89bc60394b13b54c278e3c1870890d20f79
mayflight <18868824465@163.com>
2018-09-05 15:26:31 +0800

    rename

--- src/main/java/y/y/d/springboot/service/dubbo/YyPayServiceImpl.java
+++ src/main/java/y/y/d/springboot/service/dubbo/YyPayServiceImpl.java
@@ -3,14 +3,14 @@ package y.y.d.springboot.service.dubbo;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.yadong.ye.bean.DealOrderResult;
 import com.yadong.ye.bean.Order;
-import com.yadong.ye.dubbo.YYPayService;
+import com.yadong.ye.dubbo.YyPayService;
 import org.springframework.stereotype.Component;
 /**
  * @author Ton
  */
 @Component
 @Service(timeout = 30000, delay = -1)
-public class YyPayServiceImpl implements YYPayService {
+public class YyPayServiceImpl implements YyPayService {
     @Override
     public DealOrderResult dealOrder(Order order) {
         DealOrderResult result = new DealOrderResult();
