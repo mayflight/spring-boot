b5c1fcbf9378dd65cac2570ea44a7dbd2210579c
mayflight <18868824465@163.com>
2018-09-14 11:15:09 +0800

    删除

--- src/main/java/y/y/d/springboot/service/dubbo/YyPayServiceImpl.java
+++ src/main/java/y/y/d/springboot/service/dubbo/YyPayServiceImpl.java
@@ -4,17 +4,12 @@ import com.alibaba.dubbo.config.annotation.Service;
 import com.yadong.ye.bean.DealOrderResult;
 import com.yadong.ye.bean.Order;
 import com.yadong.ye.dubbo.YyPayService;
-import org.springframework.stereotype.Component;
-
 /**
  * @author Ton
  */
 //@Component
 @Service(timeout = 30000,
         delay = -1)
-//        application = "${dubbo.application.id}",
-//        protocol = "${dubbo.protocol.id}",
-//        registry = "${dubbo.registry.id}")
 public class YyPayServiceImpl implements YyPayService {
     @Override
     public DealOrderResult dealOrder(Order order) {
